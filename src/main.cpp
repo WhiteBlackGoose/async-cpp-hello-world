f68b3f82ee230570250078a024b0c2ff52f2fc0c
WhiteBlackGoose <wbg@angouri.org>
2025-03-22 21:57:12 +0100

    minor renames

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <folly/coro/Collect.h>
 #include <folly/experimental/coro/Task.h>
 #include <folly/experimental/coro/Collect.h>
 #include <folly/experimental/coro/BlockingWait.h>
@@ -17,9 +18,9 @@ folly::coro::Task<void> doWork(int id, std::chrono::milliseconds delay) {
 
 folly::coro::Task<void> runAll() {
     co_await folly::coro::collectAll(
-        doWork(1, 1000ms),
-        doWork(2, 500ms),
-        doWork(3, 1500ms)
+        doWork(1, 9000ms),
+        doWork(2, 5000ms),
+        doWork(3, 11000ms)
     );
 }
 
