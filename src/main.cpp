1b7e9ca55b82ea58912e14849a2c4707bf79b3c3
WhiteBlackGoose <wbg@angouri.org>
2025-03-23 02:08:07 +0100

    channels

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <folly/coro/Collect.h>
+#include <folly/coro/Mutex.h>
 #include <folly/experimental/coro/Task.h>
 #include <folly/experimental/coro/Collect.h>
 #include <folly/experimental/coro/BlockingWait.h>
 #include <folly/futures/Future.h>
 #include <folly/init/Init.h> 
+#include <folly/channels/Channel.h>
 
 #include <chrono>
 #include <iostream>
+#include <memory>
+#include <string>
 
 using namespace std::chrono_literals;
 
@@ -16,12 +20,37 @@ folly::coro::Task<void> doWork(int id, std::chrono::milliseconds delay) {
     std::cout << "Task " << id << " finished after " << delay.count() << "ms\n";
 }
 
+folly::coro::Task<void> generator(std::shared_ptr<folly::channels::Sender<std::string>> writer, std::string message) {
+    for (int i = 0; i < 10; i++) {
+        co_await folly::futures::sleep(200ms);
+        writer->write(message);
+    }
+    co_return;
+}
+
+folly::coro::Task<void> consumer(folly::channels::Receiver<std::string> reader) {
+    while (auto message = co_await reader.next()) {
+        if (message->empty()) {
+            break;
+        }
+        std::cout << "Received: " << message.value() << std::endl;
+    }
+}
+
 folly::coro::Task<void> runAll() {
+    auto [receiver, sender] = folly::channels::Channel<std::string>::create();
+    auto sender_shared = std::make_shared<decltype(sender)>(std::move(sender));
     co_await folly::coro::collectAll(
         doWork(1, 900ms),
         doWork(2, 500ms),
         doWork(3, 1100ms)
     );
+    co_await folly::coro::collectAll(
+        generator(sender_shared, std::string("🔥 Hello from 1st generator")),
+        generator(std::move(sender_shared), std::string("🫐 Hello from 2nd generator")),
+        consumer(std::move(receiver))
+    );
+    co_return;
 }
 
 int main(int argc, char** argv) {
